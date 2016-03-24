a82483804643e65019e28bf5ff62e2b34e5c0dbc
John Anderson <sontek@gmail.com>
2016-03-24 22:10:24 +0800

    add chat model and view

--- chat/models.py
+++ chat/models.py
@@ -3,11 +3,17 @@ from __future__ import unicode_literals
 from django.db import models
 from django.utils import timezone
 
+
 # Create your models here.
 
 class Room(models.Model):
-    name = models.TextField()
     label = models.TextField()
+
+    def __unicode__(self):
+		return self.label	
+	
+
+
 
 
 class Message(models.Model):
@@ -15,4 +21,15 @@ class Message(models.Model):
     handle = models.TextField()
     message = models.TextField()
     timestamp = models.DateTimeField(default=timezone.now, db_index=True)
+
+	def __unicode__(self):
+		return "{timestamp} -- {handle} -- {message}".format(**self.as_dict)
+
+	@property
+	def formatted_timestmap(self):
+		return self.timestamp.strftime("%b %d %I:%M %p") 
+
+	def as_dict(self):
+		return {'handle': self.handle, 'message': self.message, 'timestamp':
+			self.timestamp}
 
--- chat/views.py
+++ chat/views.py
@@ -1,3 +1,35 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+from django.db import transaction
+from django.shortcuts import redirect, render
+from model import Room, Message
+
+import haikunator
+
+
+
+def new_room(request):
+    """create a new room and redict to it"""
+    new_room = None
+
+    while not new_room:
+        with transaction.atomic():
+            label = haikunator.haikunate()  
+            if Room.objects.filter(label=label).exists():
+                continue
+            new_room = Room.objects.create(label=label)
+    return redirect(chat_room, label=label)
+
+
+def chat_room(request, label):
+    """show the lastest messages""" 
+
+    room_object = Room.objects.filter(label=label)[0]
+    messages_list = reversed(room.messages.order_by('-timestamp')[50])
+
+    context = {
+        'room': room,
+        'messages': messages_list
+    }
